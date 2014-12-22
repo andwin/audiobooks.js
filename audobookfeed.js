340fd1a2a1aa7f0c99c3617b0d6aaf20dcd6d775
Anders Windelhed <anders@windelhed.se>
2014-12-22 16:03:14 +0100

    Broke out configuration of port number and directory for audio books

--- audobookfeed.js
+++ audobookfeed.js
@@ -5,8 +5,11 @@ var express = require('express'),
     path = require('path'),
     fs = require('fs');
 
+app.set('port', process.env.PORT || 3000);
+app.set('audiobooks_dir', process.env.AUDIOBOOKS_DIR || 'audiobooks');
+
 app.get('/', function(req, res) {
-  fs.readdir('audiobooks', function(err, files) {
+  fs.readdir(app.get('audiobooks_dir'), function(err, files) {
     var str = '';
     files.forEach(function(file) {
       if(path.extname(file) === '.mp3') {
@@ -18,4 +21,4 @@ app.get('/', function(req, res) {
   });
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(app.get('port'));
